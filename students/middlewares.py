e11ae608aa3f48aec4c0d41b741e52248bf2b294
Mohammad Jayeed <thisisjayeed@gmail.com>
2024-10-15 18:23:19 +0600

    image preview - middleware flow

--- students/middlewares.py
+++ students/middlewares.py
@@ -1,14 +1,16 @@
+from django.http import HttpResponse
 # function based middlware
 
-# def custom_function_middleware(get_response):
-#     print('this is a one time conf')
+def custom_function_middleware(get_response):
+    print('this is a one time conf')
 
-#     def middlware(request):
-#         print('this executed before the view happened')
-#         response = get_response(request)
-#         print('this executed after the view happened')
-#         return response
-#     return middlware
+    def middleware(request):
+        print('this executed FBM before the view happened')
+        response = get_response(request)
+        # response = HttpResponse('this is the response from function based middleware')
+        print('this executed FBM after the view happened')
+        return response
+    return middleware
 
 
 # class based middlware
@@ -21,6 +23,7 @@ class CustomClassBasedMiddlware:
     def __call__(self,request):
         print('this executed before the view')
         print(request.path)
-        response = self.get_response(request)
+        # response = self.get_response(request)
+        response = HttpResponse('this is the response from class based middleware')
         print('this executed after the view')
         return response
